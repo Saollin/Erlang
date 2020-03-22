ff7576ab18142e54dbd7903d565a3219c4899938
Grzegorz Janosz <gregory.janosz@gmail.com>
2020-03-23 00:44:19 +0100

    Replace pom/2 as onp/2

--- Lab1/src/onpCalculator.erl
+++ Lab1/src/onpCalculator.erl
@@ -13,25 +13,25 @@
 -export([onp/1]).
 
 onp(List) when is_list(List) ->
-  [Result] = lists:foldl(fun pom/2, [], string:tokens(List, " ")),
+  [Result] = lists:foldl(fun onp/2, [], string:tokens(List, " ")),
   Result.
 
-pom("-", [X, Y | Stack])  -> [(Y - X) | Stack];
-pom("+", [X, Y | Stack])  -> [(X + Y) | Stack];
-pom("*", [X, Y | Stack])  -> [(X * Y) | Stack];
-pom("/", [X, Y | Stack])  -> [(Y / X) | Stack];
-pom("^", [X, Y | Stack])  -> [(math:pow(Y, X)) | Stack];
-pom("pow", [X, Y | Stack])  -> [(math:pow(Y, X)) | Stack];
-pom("sqrt", [X | Stack])  -> [(math:sqrt(X)) | Stack];
-pom("sin", [X | Stack])   -> [(math:sin(X)) | Stack];
-pom("cos", [X | Stack])   -> [(math:cos(X)) | Stack];
-pom("tg", [X | Stack])    -> [(math:tan(X)) | Stack];
-pom("tan", [X | Stack])   -> [(math:tan(X)) | Stack];
-pom("ctg", [X | Stack])   -> [(1.0 / math:tan(X)) | Stack];
-pom("double", [X | Stack]) -> [(2 * X) | Stack];
-pom("doublesum", [X, Y | Stack]) -> [2 * (X + Y) | Stack];
+onp("-", [X, Y | Stack])  -> [(Y - X) | Stack];
+onp("+", [X, Y | Stack])  -> [(X + Y) | Stack];
+onp("*", [X, Y | Stack])  -> [(X * Y) | Stack];
+onp("/", [X, Y | Stack])  -> [(Y / X) | Stack];
+onp("^", [X, Y | Stack])  -> [(math:pow(Y, X)) | Stack];
+onp("pow", [X, Y | Stack])  -> [(math:pow(Y, X)) | Stack];
+onp("sqrt", [X | Stack])  -> [(math:sqrt(X)) | Stack];
+onp("sin", [X | Stack])   -> [(math:sin(X)) | Stack];
+onp("cos", [X | Stack])   -> [(math:cos(X)) | Stack];
+onp("tg", [X | Stack])    -> [(math:tan(X)) | Stack];
+onp("tan", [X | Stack])   -> [(math:tan(X)) | Stack];
+onp("ctg", [X | Stack])   -> [(1.0 / math:tan(X)) | Stack];
+onp("double", [X | Stack]) -> [(2 * X) | Stack];
+onp("doublesum", [X, Y | Stack]) -> [2 * (X + Y) | Stack];
 
-pom(Num, Stack)           -> [parseNumber(Num) | Stack].
+onp(Num, Stack)           -> [parseNumber(Num) | Stack].
 
 parseNumber(Num) ->
   case string:to_float(Num) of
